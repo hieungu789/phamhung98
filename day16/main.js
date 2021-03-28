ead84aa92cc93060102ecd2348c333c879f1085f
hieungu789 <60737506+hieungu789@users.noreply.github.com>
2021-03-28 22:08:31 +0700

    bs

--- day16/main.js
+++ day16/main.js
@@ -1,18 +1,26 @@
 
 function quyDoiTienTe() {
+    let moneyOutput = document.getElementById("outputMoney")
     let moneyInput = document.getElementById("numberAcheck").value;
     let moneyOrigin = document.getElementById("from").value
     let moneyAfter = document.getElementById("to").value
-    let moneyOutput = document.getElementById("outputMoney")
-    if(moneyOutput<0||!Number.isInteger("numberAcheck")){
-        moneyOutput.innerHTML = `Giá Trị phải lớn hơn hoặc bằng 0`;
-    }else {
+    if (moneyInput < 0 && moneyOrigin === "USD" && moneyAfter === "USD" ||
+        moneyInput < 0 && moneyOrigin === "USD" && moneyAfter === "VND" ||
+        moneyInput < 0 && moneyOrigin === "USD" && moneyAfter === "EURO" ||
+        moneyInput < 0 && moneyOrigin === "VND" && moneyAfter === "USD" ||
+        moneyInput < 0 && moneyOrigin === "VND" && moneyAfter === "VND" ||
+        moneyInput < 0 && moneyOrigin === "VND" && moneyAfter === "EURO" ||
+        moneyInput < 0 && moneyOrigin === "EURO" && moneyAfter === "EURO" ||
+        moneyInput < 0 && moneyOrigin === "EURO" && moneyAfter === "VND" ||
+        moneyInput < 0 && moneyOrigin === "EURO" && moneyAfter === "USD" ) {
+        moneyOutput.innerHTML = "Giá Trị lớn hơn 0 hoặc bằng 0"
+    } else {
         if (moneyOrigin === "USD" && moneyAfter === "VND") {
-            moneyOutput.innerHTML = `${moneyInput * 23.085} VND`;
+            moneyOutput.innerHTML = `${moneyInput * 23085} VND`;
         } else if (moneyOrigin === "USD" && moneyAfter === "EURO") {
             moneyOutput.innerHTML = `${moneyInput * 0.85} EURO`;
         } else if (moneyOrigin === "USD" && moneyAfter === "USD") {
-            moneyOutput.innerHTML = `${moneyInput * 23.085} USD`;
+            moneyOutput.innerHTML = `${moneyInput * 1} USD`;
         }
         if (moneyOrigin === "EURO" && moneyAfter === "VND") {
             moneyOutput.innerHTML = `${moneyInput * 27400} VND;`
@@ -27,8 +35,9 @@ function quyDoiTienTe() {
             let a = moneyInput / 27400;
             moneyOutput.innerHTML = `${Math.round(a * 100) / 100} EURO`;
         } else if (moneyOrigin === "VND" && moneyAfter === "USD") {
-            let b =moneyInput / 23085;
+            let b = moneyInput / 23085;
             moneyOutput.innerHTML = `${Math.round(b * 100) / 100} USD`;
         }
+
     }
 }
